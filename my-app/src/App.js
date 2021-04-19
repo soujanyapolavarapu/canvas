d242e6204e9fb4435ab8ad4be928ed60038f58b6
soujanya polavarapu <soujanyabonthu@gmail.com>
2021-04-19 16:47:33 +0200

    added canvas files

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FindTriangleName from './FindTriangleName';
+
+function App() {
+  return (
+    <div className="App">
+      <FindTriangleName/>
+    </div>
+  );
+}
+
+export default App;
